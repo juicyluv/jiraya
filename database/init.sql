5eac4b1c8ccd26307de7a63cc3a6ef5067ade9d5
= <=>
2022-04-12 01:25:39 +0300

    Still experimenting with postgres plpgsql functions.

--- database/init.sql
+++ database/init.sql
@@ -1,18 +1,31 @@
 create schema if not exists main;
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 create table if not exists users(
     id uuid primary key,
-    title text
+    username text not null unique,
+    password text not null,
+    created_at timestamptz default now(),
+    disabled_at timestamptz
 );
 
-create or replace function main.create_user(username varchar)
-    returns varchar
+create or replace function main.create_user(username text, password text)
+    returns uuid
 as $$
 
+declare
+    id uuid;
+    hashed_password text;
+
 begin
+    id = uuid_generate_v4();
+    hashed_password = md5(password);
 
-    return 'hello world';
+    insert into users(id, username, password)
+        values(id, username, hashed_password);
 
+    return id;
 end;
 
 $$
