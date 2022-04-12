21a77a18f5e0aa57b07b72767ab633cd408027c5
sokolov <sokolov@trueconf.ru>
2022-04-12 17:01:58 +0300

    Updated init sql.

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,7 @@
 create schema if not exists main;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 create table if not exists users(
     id uuid primary key,
@@ -10,7 +11,7 @@ create table if not exists users(
     disabled_at timestamptz
 );
 
-create or replace function main.create_user(username text, password text)
+create or replace function main.create_user(_username text, _password text)
     returns uuid
 as $$
 
@@ -19,14 +20,45 @@ declare
     hashed_password text;
 
 begin
-    id = uuid_generate_v4();
-    hashed_password = md5(password);
+    id = gen_random_uuid();
+    hashed_password = crypt(_password::varchar, gen_salt('bf', 8));
 
     insert into users(id, username, password)
-        values(id, username, hashed_password);
+        values(id, _username, hashed_password);
 
     return id;
 end;
 
 $$
 language plpgsql;
+
+create or replace function main.get_user(_id uuid, _username text default null::text, OUT username text, OUT password text, OUT error jsonb) returns record
+    stable
+    strict
+    language plpgsql
+as
+$$
+begin
+
+    select
+        u.username,
+        u.password
+    into
+        username,
+        password
+    from users u
+    where u.id = _id and u.username = _username;
+
+    if not found then
+        error := jsonb_build_object('code', 1, 'detail', json_build_object('user_id', 5));
+        return;
+    end if;
+
+    error := jsonb_build_object('code', 0);
+
+    exception
+        when others then
+            error := jsonb_build_object('code', -1);
+
+end;
+$$
