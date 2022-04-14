c9af87b34614d88c8d2810dcade11505600b8cc7
juicyluv <juicyluv@mail.ru>
2022-04-15 00:53:04 +0300

    Described user contacts requests.

--- internal/jiraya/domain/user_contact.go
+++ internal/jiraya/domain/user_contact.go
@@ -20,3 +20,21 @@ type GetUserContactRequest struct {
 type GetUserContactResponse struct {
 	Contact *UserContact `json:"contact"`
 }
+
+type GetUserContactsRequest struct {
+	UserID string `json:"user_id"`
+}
+
+type GetUserContactsResponse struct {
+	Contacts []*UserContact `json:"contacts"`
+}
+
+type UpdateUserContactRequest struct {
+	ContactID   string  `json:"contact_id"`
+	ContactName *string `json:"contact_name"`
+	Contact     *string `json:"contact"`
+}
+
+type DeleteUserContactRequest struct {
+	ContactID string `json:"contact_id"`
+}
